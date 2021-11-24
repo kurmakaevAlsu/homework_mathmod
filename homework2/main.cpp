cd75c4397e35f95a99db3e07825329944b22df4e
kurmakaevAlsu <93134368+kurmakaevAlsu@users.noreply.github.com>
2021-11-24 21:34:45 +0500

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -11,37 +11,30 @@ int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
 
-
 const double g = 9.81;
 
 
 
 int main(int argc, char** argv)
 {
-
     string input;
     try {
 
 
         if (argc == 2)
         {
-
             input = argv[1];
         }
         else {
-
             throw invalid_argument("wrong number of arguments");
-
         }
     }
     catch (invalid_argument e) {
-
         std::cout << e.what() << endl;
         return 0;
     }
 
      ifstream cin(input);
-
 
     double h0;
     cin >> h0;
@@ -59,6 +52,7 @@ int main(int argc, char** argv)
         xis.push_back(xi);
         his.push_back(hi);
     }
+
 
 
     while (x > xis[ans]) {
@@ -81,6 +75,7 @@ int main(int argc, char** argv)
 
 
 
+
         double dt = (xis[next] - x) / vx;
         y = y + vy * dt - g * dt * dt / 2;
 
@@ -88,13 +83,13 @@ int main(int argc, char** argv)
             std::cout << ans;
             return 0;
         }
-        else if (y >= his[next]) {
+        else if (y > his[next]) {
 
             x = xis[next];
 
             ans += dir;
         }
-        else if (y < his[next]) {
+        else if (y <= his[next]) {
 
             vx = -vx;
             x = xis[next];
