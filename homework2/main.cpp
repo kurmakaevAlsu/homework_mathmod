5d79315a0fba46eeb213497e59b7bf70cbd5b91f
kurmakaevAlsu <93134368+kurmakaevAlsu@users.noreply.github.com>
2021-11-24 21:43:12 +0500

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -11,55 +11,49 @@ int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
 
-
 const double g = 9.81;
 
 
 
 int main(int argc, char** argv)
 {
-
     string input;
     try {
 
 
         if (argc == 2)
         {
-
             input = argv[1];
         }
         else {
-
             throw invalid_argument("wrong number of arguments");
-
         }
     }
     catch (invalid_argument e) {
-
         std::cout << e.what() << endl;
         return 0;
     }
 
      ifstream cin(input);
 
-
     double h0;
     cin >> h0;
+
     double vx0, vy0;
     cin >> vx0 >> vy0;
+
     double xi, hi;
     vector<double> xis;
     vector<double> his;
 
+
     double y = h0, t = 0, x = 0.0, vx = vx0, vy = vy0;
     int ans = 0;
-
 
     while (cin >> xi >> hi) {
         xis.push_back(xi);
         his.push_back(hi);
     }
-
 
     while (x > xis[ans]) {
         ans++;
@@ -67,19 +61,14 @@ int main(int argc, char** argv)
 
 
     while (true) {
-
         int dir = sgn(vx);
 
         int next = (dir > 0) ? ans : ans - 1;
-
-
 
         if (next < 0 || next >= xis.size()) {
             cout << ans;
             return 0;
         }
-
-
 
         double dt = (xis[next] - x) / vx;
         y = y + vy * dt - g * dt * dt / 2;
@@ -97,6 +86,7 @@ int main(int argc, char** argv)
         else if (y < his[next]) {
 
             vx = -vx;
+
             x = xis[next];
 
         }
