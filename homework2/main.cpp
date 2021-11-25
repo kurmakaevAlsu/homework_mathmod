338404a90d84de696f5ce7aceaf0c51f59589ce3
kurmakaevAlsu <93134368+kurmakaevAlsu@users.noreply.github.com>
2021-11-25 20:13:37 +0500

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-template <typename T>
+template <typename T> 
 int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
@@ -38,63 +38,73 @@ int main(int argc, char** argv)
 
     double h0;
     cin >> h0;
-
     double vx0, vy0;
     cin >> vx0 >> vy0;
-
     double xi, hi;
     vector<double> xis;
     vector<double> his;
 
-
     double y = h0, t = 0, x = 0.0, vx = vx0, vy = vy0;
     int ans = 0;
 
+
     while (cin >> xi >> hi) {
         xis.push_back(xi);
-        his.push_back(hi);
+        his.push_back(hi);       
     }
 
     while (x > xis[ans]) {
         ans++;
     }
 
-
+    
     while (true) {
+      
         int dir = sgn(vx);
-
+   
         int next = (dir > 0) ? ans : ans - 1;
+        
+   
 
         if (next < 0 || next >= xis.size()) {
             cout << ans;
             return 0;
         }
 
+
+        
         double dt = (xis[next] - x) / vx;
+       
         y = y + vy * dt - g * dt * dt / 2;
 
+       
         if (y <= 0.0) {
             std::cout << ans;
             return 0;
-        }
+        } 
         else if (y >= his[next]) {
-
-            x = xis[next];
-
+           
+            
+            
+            x = xis[next];            
+            
+            
             ans += dir;
         }
         else if (y < his[next]) {
-
-            vx = -vx;
-
+           
+            vx = -vx;  
+          
             x = xis[next];
-
+            
         }
+     
         vy = vy - g * dt;
         t += dt;
 
-    }
+    }    
     return 0;
 }
+
 
 
